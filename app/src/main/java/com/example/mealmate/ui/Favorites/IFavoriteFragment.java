8b5c12b57c135f282f0114b13032ea118a446084
Mohamed taj Eldin Ahmed <mohamedtajeldinai@gmail.com>
2025-02-15 12:17:51 +0200

    Add favorites View component

---
+++ app/src/main/java/com/example/mealmate/ui/Favorites/IFavoriteFragment.java
@@ -0,0 +1,9 @@
+package com.example.testauth.ui.Favorites;
+
+import com.example.testauth.Models.MealDto;
+
+import java.util.List;
+
+public interface IFavoriteFragment {
+    public void showdata(List<MealDto> meals);
+}
